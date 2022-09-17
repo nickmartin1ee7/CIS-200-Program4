e6ab8dc7118d2728d563ac19de6e0fdb89016756
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-09-17 16:13:46 -0400

    Add unit test proj

---
+++ Prog0.Tests/GroundPackageTests.cs
@@ -0,0 +1,16 @@
+namespace Prog0.Tests
+{
+    public class GroundPackageTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Prog0.Tests/TwoDayAirPackage.cs
@@ -0,0 +1,16 @@
+namespace Prog0.Tests
+{
+    public class TwoDayAirPackage
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
