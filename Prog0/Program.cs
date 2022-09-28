0aeaf10b237ce8d2c1d46c31a23037b58c0e63e6
Nick <nickmartin1ee7@gmail.com>
2022-09-28 18:15:49 -0400

    Implement LINQ queries

--- Prog0/Program.cs
+++ Prog0/Program.cs
@@ -1,16 +1,9 @@
 ﻿/* Author: Nick Martin (5111752)
- * Program: 1A
- * Due: 09/26/2022
+ * Program: 1B
+ * Due: 10/08/2022
  * 
  * Notes:
  *    Tests are performed in Prog0.Tests
- * 
- * New Class Descriptions in Prog0.ClassLib.Models.New namespace:
- *    Package - An abstract class that build off of Parcel, and additionally specify dimentions and weight for a package.
- *    GroundPackage - A concrete class that builds off of Package, and includes a calculated ZoneDistance that is used when determining cost.
- *    AirPackage - An abstract class that builds off of Package, and includes methods to determine if package is heavy or long.
- *    NextDayAirPackage - A concrete class that builds off of AirPackage, and includes an express fee that is used when determining cost as well as surcharges regarding heavy or large package dimentions.
- *    TwoDayAirPackage - A concrete class that builds off of AirPackage, and includes Delivery enum that is used when determining cost as well as an applicable saver discount to the total cost.
  */
 
 using Prog0.ClassLib.Models.New;
@@ -25,41 +18,74 @@ class Program
     // Postcondition: Small list of Parcels is created and displayed
     static void Main(string[] args)
     {
-        Address a1 = new Address("  John Smith  ", "   123 Any St.   ", "  Apt. 45 ",
-            "  Louisville   ", "  KY   ", 40202); // Test Address 1
-        Address a2 = new Address("Jane Doe", "987 Main St.",
-            "Beverly Hills", "CA", 90210); // Test Address 2
-        Address a3 = new Address("James Kirk", "654 Roddenberry Way", "Suite 321",
-            "El Paso", "TX", 79901); // Test Address 3
-        Address a4 = new Address("John Crichton", "678 Pau Place", "Apt. 7",
-            "Portland", "ME", 04101); // Test Address 4
-
-        Letter l1 = new Letter(a1, a3, 0.50M); // Test Letter 1
-        Letter l2 = new Letter(a2, a4, 1.20M); // Test Letter 2
-        Letter l3 = new Letter(a4, a1, 1.70M); // Test Letter 3
-
-        List<Parcel> parcels = new List<Parcel>
+        var addresses = new List<Address>
         {
-            l1,
-            l2,
-            l3,
-            new GroundPackage(a1, a2, 100, 100, 100, 100),
-            new GroundPackage(a1, a2, 1, 1, 1, 1),
-            new NextDayAirPackage(a3, a4, 5, 5, 5, 5, 250.99M),
-            new NextDayAirPackage(a3, a4, 100, 100, 100, 100, 250.99M),
-            new TwoDayAirPackage(a3, a4, 5, 5, 5, 5, TwoDayAirPackage.Delivery.Early),
-            new TwoDayAirPackage(a3, a4, 5, 5, 5, 5, TwoDayAirPackage.Delivery.Saver),
-            new TwoDayAirPackage(a3, a4, 100, 100, 100, 100, TwoDayAirPackage.Delivery.Early),
-            new TwoDayAirPackage(a3, a4, 100, 100, 100, 100, TwoDayAirPackage.Delivery.Saver)
+            new Address("John Smith", "123 Any St.", "Apt. 45", "Louisville", "KY", 40000),
+            new Address("Smith Smith", "123 Any St.", "Apt. 45", "Louisville", "KY", 40202),
+            new Address("Jane Doe", "987 Main St.", "Beverly Hills", "CA", 90210),
+            new Address("James Kirk", "654 Roddenberry Way", "Suite 321", "El Paso", "TX", 79901),
+            new Address("John Crichton", "678 Pau Place", "Apt. 7", "Portland", "ME", 04101),
+            new Address("Crichton John", "123 Any St.", "Apt. 45", "Louisville", "KY", 40204),
+            new Address("Jane Doe", "987 Main St.", "Beverly Hills", "CA", 90290),
+            new Address("James Kirk", "654 Roddenberry Way", "Suite 321", "El Paso", "TX", 79902),
+        };
+
+        var parcels = new List<Parcel>
+        {
+            new Letter(addresses[0], addresses[1], 0.50M),
+            new Letter(addresses[2], addresses[3], 1.20M),
+            new Letter(addresses[3], addresses[1], 1.70M),
+            new GroundPackage(addresses[0], addresses[1], 100, 100, 100, 100),
+            new GroundPackage(addresses[0], addresses[1], 1, 1, 1, 1),
+            new NextDayAirPackage(addresses[2], addresses[3], 5, 5, 5, 5, 250.99M),
+            new NextDayAirPackage(addresses[2], addresses[3], 100, 100, 100, 100, 250.99M),
+            new TwoDayAirPackage(addresses[2], addresses[3], 5, 5, 5, 5, TwoDayAirPackage.Delivery.Early),
+            new TwoDayAirPackage(addresses[2], addresses[3], 5, 5, 5, 5, TwoDayAirPackage.Delivery.Saver),
+            new TwoDayAirPackage(addresses[2], addresses[3], 100, 100, 100, 100, TwoDayAirPackage.Delivery.Early),
+            new TwoDayAirPackage(addresses[2], addresses[3], 100, 100, 100, 100, TwoDayAirPackage.Delivery.Saver),
+            new Letter(addresses[4], addresses[5], 0.50M),
+            new Letter(addresses[6], addresses[7], 1.20M),
+            new Letter(addresses[7], addresses[4], 1.70M),
+            new GroundPackage(addresses[4], addresses[5], 100, 100, 100, 100),
+            new GroundPackage(addresses[4], addresses[5], 1, 1, 1, 1),
+            new NextDayAirPackage(addresses[6], addresses[7], 5, 5, 5, 5, 250.99M),
+            new NextDayAirPackage(addresses[6], addresses[7], 100, 100, 100, 100, 250.99M),
+            new TwoDayAirPackage(addresses[6], addresses[7], 5, 5, 5, 5, TwoDayAirPackage.Delivery.Early),
+            new TwoDayAirPackage(addresses[6], addresses[7], 5, 5, 5, 5, TwoDayAirPackage.Delivery.Saver),
+            new TwoDayAirPackage(addresses[6], addresses[7], 100, 100, 100, 100, TwoDayAirPackage.Delivery.Early),
+            new TwoDayAirPackage(addresses[6], addresses[7], 100, 100, 100, 100, TwoDayAirPackage.Delivery.Saver)
         }; // Test list of parcels
 
         // Display data
-        WriteLine("Program 1A - List of Parcels\n");
+        WriteLine("Program 1B - List of Parcels\n");
 
         foreach (Parcel p in parcels)
         {
             WriteLine(p);
             WriteLine("--------------------");
         }
+
+        Console.WriteLine("# Select all Parcels and order by destination zip (descending)");
+        Console.WriteLine(string.Join(Environment.NewLine, from p in parcels
+                                                           orderby p.DestinationAddress.Zip descending
+                                                           select p));
+        
+        Console.WriteLine("# Select all Parcels and order by cost (ascending)");
+        Console.WriteLine(string.Join(Environment.NewLine, from p in parcels
+                                                           orderby p.CalcCost() ascending
+                                                           select p));
+        
+        Console.WriteLine("# Select all Parcels and order by Parcel type (ascending) and then cost (descending)");
+        Console.WriteLine(string.Join(Environment.NewLine, from p in parcels
+                                                           orderby p.GetType().Name ascending
+                                                           orderby p.CalcCost() descending
+                                                           select p));
+
+        Console.WriteLine("# Select all AirPackage objects that are heavy and order by weight (descending)");
+        Console.WriteLine(string.Join(Environment.NewLine, from p in parcels
+                                                           where p is AirPackage airP && airP.IsHeavy()
+                                                           let airP = (AirPackage)p
+                                                           orderby airP.Weight descending
+                                                           select airP));
     }
 }
